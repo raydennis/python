5c4db3efd1e29fa03735a46622160dc507c4cbfc
Ray Dennis <raydennis@outlook.com>
2019-01-04 13:18:47 -0700

    moved to pep8 from pylint standards

--- examples/classes.py
+++ examples/classes.py
@@ -1,13 +1,43 @@
+import unittest
+
+
 class BankAccount(object):
     def __init__(self, initial_balance=0):
         self.balance = initial_balance
+
     def deposit(self, amount):
         self.balance += amount
+
     def withdraw(self, amount):
         self.balance -= amount
+
     def overdrawn(self):
         return self.balance < 0
-my_account = BankAccount(15)
-my_account.withdraw(5)
-print (my_account.balance)
-  
+
+
+class TestBankAccount(unittest.TestCase):
+    def test_bank_account_deposit(self):
+        account = BankAccount(0)
+        account.deposit(5)
+        self.assertEqual(account.balance, 5)
+
+    def test_bank_account_withdraw(self):
+        account = BankAccount(5)
+        account.withdraw(5)
+        self.assertEqual(account.balance, 0)
+
+    def test_bank_account_overdrawn_true(self):
+        account = BankAccount(-5)
+        self.assertTrue(account.overdrawn())
+
+    def test_bank_account_overdrawn_false(self):
+        account = BankAccount(5)
+        self.assertFalse(account.overdrawn())
+
+
+class testIng(unittest.TestCase):
+    def WhateverIWannaDo(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
--- examples/openingFiles.py
+++ examples/openingFiles.py
@@ -1,12 +1,13 @@
-# indent your Python code to put into an email
 import glob
 # glob supports Unix style pathname extensions
+
 python_files = glob.glob('*.py')
 for file_name in sorted(python_files):
-    print '    ------' + file_name
+    print('    ------' + file_name)
 
     with open(file_name) as f:
         for line in f:
-            print '    ' + line.rstrip()
+            # rstrip removes [arg] from the end of the string
+            print('    ' + line.rstrip())
 
     print
