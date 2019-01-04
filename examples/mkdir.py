4dc30ee66fd8984b6a84b8f9afffc7c3f72e70c0
Ray Dennis <raydennis@outlook.com>
2019-01-04 13:18:31 -0700

    added directory example mkdir.py

--- examples/mkdir.py
+++ examples/mkdir.py
@@ -1,4 +1,35 @@
-import os, sys
+'''
+playing with directories in Python
 
-for x in range(0,2):
-    os.mkdir(str(x))
+
+the inspect module can be used to get information on specific files
+but you can also use __file__ if you're only looking for
+the current running directory
+'''
+
+import os
+import unittest
+from pathlib import Path
+# from inspect import currentframe, getframeinfo
+
+
+def make_directory(directory):
+    os.mkdir(directory)
+    if Path(directory).exists():
+        # following lines needed if you're using inspect module
+        # filename = getframeinfo(currentframe()).filename
+        # parent = Path(filename).resolve().parent
+
+        parent = str(Path(__file__).resolve().parent)
+        print("directory created: " + parent + "/" + directory)
+
+
+class TestMakeDirectory(unittest.TestCase):
+    def test_make_directory(self):
+        directory = "testMakeDirectory"
+        make_directory(directory)
+        self.assertTrue(Path(directory).exists())
+        os.rmdir(directory)
+
+if __name__ == "__main__":
+    unittest.main()
