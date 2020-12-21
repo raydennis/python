2f8660b02b6c0a0bb2a1ce1ddcb5457f987c913d
Ray Dennis <raydennis@outlook.com>
2020-12-21 11:23:28 -0700

    Upadated makefile example with newer commands

--- examples/makefile.py
+++ examples/makefile.py
@@ -1,4 +1,16 @@
-import os
+# OLD way
+# import os
 
-for x in range(0, 5):
-    os.system("touch test" + str(x) + ".txt")
+# for x in range(0, 5):
+#     os.system("touch test" + str(x) + ".txt")
+
+
+# updated way
+from pathlib import Path
+from posix import listdir
+
+# create the file
+Path('file.txt').touch()
+
+# call list directory
+# exec(open("listdir.py").read())
